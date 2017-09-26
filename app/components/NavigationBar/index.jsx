362c20c40baec6d4e192a831a3eb64559e319ccb
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-09-25 23:51:42 -0700

    New component.

---
+++ app/components/NavigationBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FilterableNavigationBar() {
+  return (
+    <ul>
+      <li>amin</li>
+      <li>ogarrio</li>
+      <li>Cecena</li>
+    </ul>
+  );
+}
+
+export default FilterableNavigationBar;
